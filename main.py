5a2e04876ca191c8e6fbc7cbac693168ad7a3d67
Anna Zubova <anna@leolog.com>
2019-09-21 11:44:03 +0200

    Correct mistake in function name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from functions import percentage_of_word
+from functions import percentage_of_word_in_localfile
 
 if __name__ == "__main__":
 
     # Print out ratio of 'and' occurrences to the total number of words in the 'ufo.txt'
-    print(percente_of_word_in_localfile('and', 'ufo.txt'))
+    print(percentage_of_word_in_localfile('and', 'ufo.txt'))
