6921d97d8c683bb97622225587a066f07d70af9a
Anna Zubova <anna@leolog.com>
2019-09-21 12:44:56 +0200

    Correct docstring text, update README

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 This repository contains code used in my blog posts on SOLID principles.
 
+File structure:
+
+- `functions.py`: SOLID-friendly code
+- Use `main.py` to execute functions from `functions.py`
+- `wrong.py`: code designed against SOLID principles.
+- `tests.py` contains unit tests for functions from `functions.py`
+
 Blog post links:
 
-[Single Responsibility principle]()
+[Single Responsibility principle](https://dev.to/annalara/solid-programming-part-1-single-responsibility-principle-1ki6)
--- functions.py
+++ functions.py
@@ -19,12 +19,13 @@ def count_word_occurrences(word, content):
 
 
 def percentage_of_word(word, content):
-    '''Calculate ratio of number of word occurrences to number of all words'''
+    '''Calculate ratio of number of word occurrences to number of all words in a text'''
     total_words = number_of_words(content)
     word_occurrences = count_word_occurrences(word, content)
     return word_occurrences/total_words
 
 
 def percentage_of_word_in_localfile(word, file):
+    '''Calculate ratio of number of word occurrences to number of all words in a text file'''
     content = read_localfile(file)
     return percentage_of_word(word, content)
