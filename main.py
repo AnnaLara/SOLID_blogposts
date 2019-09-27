310c7dad03f57fb81bde888788e0a386438cc380
Anna Zubova <anna@leolog.com>
2019-09-27 16:03:07 +0200

    Update main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from functions import percentage_of_word_in_localfile
+from functions import percentage_of_word_in_localfile, count_word_occurrences
+from functions import get_text_from_url
 
 if __name__ == "__main__":
 
     # Print out ratio of 'and' occurrences to the total number of words in the 'ufo.txt'
     print(percentage_of_word_in_localfile('and', 'ufo.txt'))
+
+    content = get_text_from_url('https://en.wikipedia.org/wiki/Main_Page')
+    count_word_occurrences('and', content)
